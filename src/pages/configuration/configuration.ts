4ad40c7d8594c4efe4a64b23ee6e1f44881e4a8b
Jhonatan Medeiros <jhonatan.medeiros02@gmail.com>
2018-09-24 19:27:17 -0300

    Adicionado confirmação para trocar de moeda

--- src/pages/configuration/configuration.ts
+++ src/pages/configuration/configuration.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 
 // Services
 import { TranslateStorageProvider } from '../../providers/translate-storage/translate-storage';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-configuration',
@@ -10,14 +11,45 @@ import { TranslateStorageProvider } from '../../providers/translate-storage/tran
 })
 export class ConfigurationPage {
 
-  language: string = this.translateStorageService.current_lang;
+  language: string;
 
   currency: string = 'brl';
 
-  constructor(private translateStorageService: TranslateStorageProvider) { }
+  constructor(
+    private translateStorageService: TranslateStorageProvider,
+    private alertCtrl: AlertController
+  ) {
+    this.language = this.translateStorageService.current_lang;
+  }
 
   changeLanguage(): void {
     this.translateStorageService.saveLocalization(this.language);
+    this.showAlert();
+  }
+
+  changeCurrency(): void {
+    switch (this.language) {
+      case 'pt': {
+        this.currency = 'brl';
+        break;
+      }
+      case 'en': {
+        this.currency = 'usd';
+        break;
+      }
+    }
+  }
+
+  showAlert(): void {
+    let alert = this.alertCtrl.create({
+      title: 'Alterar moeda',
+      message: 'Deseja alterar para a moeda local ?',
+      buttons: [
+        { text: 'Cancelar',  role: 'cancel' },
+        { text: 'Ok', handler: () => this.changeCurrency() }
+      ]
+    });
+    alert.present();
   }
 
 }
